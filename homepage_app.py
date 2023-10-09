78836f0597127349e0589068b8940b1655901417
Tyovendi Arisandy <100063943+Tyovendiarisandy@users.noreply.github.com>
2023-10-09 21:58:49 +0700

    Update homepage_app.py

--- homepage_app.py
+++ homepage_app.py
@@ -7,10 +7,10 @@ import pickle
 from PIL import Image
 
 # load dataset
-df = pd.read_csv('clean_dataset.csv')
+df = pd.read_csv('train.csv')
 
 # load model
-model = pickle.load(open('forest_fix_tuned.pkl','rb'))
+model = pickle.load(open('xgb_fix_tuned.pkl','rb'))
     
 # create title (homepage)
 def main():
@@ -21,31 +21,43 @@ def main():
 
     # choose menu input - Selectbox
     # st.sidebar.subheader('Select Your Input')
-    gender = st.selectbox('Select Your Gender!', df['gender'].unique())
-    race = st.selectbox('Select Your Race or Ethnicity!', df['race'].unique())
-    parental_level_of_education = st.selectbox("Select Your Parent's Level of Education!", df['parental_level_of_education'].unique())
-    lunch = st.selectbox('Select Your Lunch Type!', df['lunch'].unique())
-    test_preparation_course = st.selectbox('Select Your Status Test Preparation Course!', df['test_preparation_course'].unique())
+    state = st.selectbox('Select Your State!', df['state'].unique())
+    international_plan = st.selectbox('Select Your International Plan Package!', df['international_plan'].unique())
+    voice_mail_plan = st.selectbox("Select Your Voice Mail Plan Package!", df['voice_mail_plan'].unique())
 
-    # subtitle for symptoms
-    st.subheader('Select Your Exams Score!')
+    # subtitle for numerical feature
+    st.subheader('Select Your History of Telecommunications Services!')
     
     # choose menu input - selectbox for symptoms
-    math_score = st.slider('What is Your Math Score?', min_value=0, max_value=100)
-    ebrw_score = st.slider('What is Your Evidence-Based Reading and Writing Score?', min_value=0, max_value=100)
+    account_length = st.number_input('How is Your Account Length?', min_value=0, max_value=400)
+    total_day_calls = st.number_input('How is Your Total Day Calls??', min_value=0, max_value=400)
+    total_day_charge = st.number_input('How is Your Total Day Charge??', min_value=0, max_value=400)
+    total_eve_calls = st.number_input('How is Your Total Evening Calls??', min_value=0, max_value=400)
+    total_eve_charge = st.number_input('How is Your Total Evening Charge??', min_value=0, max_value=400)
+    total_night_calls = st.number_input('How is Your Total Night Calls??', min_value=0, max_value=400)
+    total_night_charge = st.number_input('How is Your Total Night Charge??', min_value=0, max_value=400)
+    total_intl_calls = st.number_input('How is Your Total International Calls??', min_value=0, max_value=400)
+    total_intl_charge = st.number_input('How is Your Total International Charge??', min_value=0, max_value=400)
+    number_customer_service_calls = st.number_input('How is Your Total Customer Service Calls??', min_value=0, max_value=400)
 
     # prediction - button for predict
     if st.button('Predict',help='Click to predict'):
         
     # input the data in dataframe
         input_data = pd.DataFrame({
-        'gender': [gender],
-        'race': [race],
-        'parental_level_of_education': [parental_level_of_education],
-        'lunch': [lunch],
-        'test_preparation_course': [test_preparation_course],
-        'math_score': [math_score],
-        'ebrw_score': [ebrw_score]
+        'state': [state],
+        'international_plan': [international_plan],
+        'voice_mail_plan': [voice_mail_plan],
+        'account_length': [account_length],
+        'total_day_calls': [total_day_calls],
+        'total_day_charge': [total_day_charge],
+        'total_eve_calls': [total_eve_calls],
+        'total_eve_charge': [total_eve_charge],
+        'total_night_calls': [total_night_calls],
+        'total_night_charge': [total_night_charge],
+        'total_intl_calls': [total_intl_calls],
+        'total_intl_calls': [total_intl_calls],
+        'number_customer_service_calls': [number_customer_service_calls]
         })
         
         # do predict with model
@@ -54,9 +66,9 @@ def main():
         st.subheader('Prediction Result')
         
         if prediction[0] == 0:
-            st.warning("You Didn't Pass The SAT Test!")
+            st.success("Customers Who Have No Potential to Churn!")
         else:
-            st.success("You Passed The SAT Test!")
+            st.warning("Customers Who Have The Potential to Churn!")
 
     
     st.write('----')
